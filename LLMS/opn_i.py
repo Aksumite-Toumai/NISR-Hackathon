7af4a9f00ed0bbb59a13902072a511c3462e7247
Luel Hagos <luelh0101@gmail.com>
2023-11-29 18:57:40 +0200

    Create opn_i.py

---
+++ LLMS/opn_i.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=openai_api_key)
+
+def chat_csv(df, question):
+    context = df.to_dict('list')
+    context = f"""You are a senior data scientist. Below is a dataset where the dataframe has been converted into a dictionary. The keys represent the column names, and the values correspond to the data in each column. Please analyze the data and answer the question based on the provided context. If the context is insufficient or irrelevant, indicate that there is not enough information available. Do not use external knowledge beyond what is presented in the dataset.
+    Don't showw how you did it, give the final answer.
+            {context}
+    """
+
+    prompt = f"{context}\n\nQuestion: {question}"
+    response = client.chat.completions.create(
+    model="gpt-4-1106-preview",
+    messages=[
+        {"role": "system", "content": context},
+        {"role": "user", "content": prompt}
+    ]
+    )
+
+    response = response.choices[0].message
+    response = response.content
+    return response
+
+df  = pd.read_csv("NIMC.csv")
+question = "Give me a summary about the population and list the top three states with the highest population."
+answer = chat_csv(df, question)
+print(answer)
